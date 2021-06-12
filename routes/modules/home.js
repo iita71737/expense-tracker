52e904f93aebab67d89340af731c899041d0ae56
danlin <iita71737@gmail.com>
2021-06-12 21:12:07 +0800

    finished sort by category and CRUD

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -38,15 +38,32 @@ try{
 catch (e) {
     console.warn(e)
 }
-});
+})
 
-router.get('/search', (req, res) => {
-    const keyWord = req.query
-    console.log(keyword)
-    Record.find( {category_en :new RegExp(keyWord, 'i')})
-    .lean()
-    .then( record => res.render('index', { record , keyWord} ))
-    .catch(error => console.error(error))
+router.get('/filter', async (req, res) => {
+
+    try{
+    const keyWord = req.query.categorySelect
+    let records = await Record.find({ category_en : keyWord }).lean()
+    let categories = await Category.find().lean()
+
+    records.forEach( record => {
+            //iconFilter get icon by compare to category
+            record.iconName  = categories.find( item => item.cateName === record.category_en ? item.cateIcon3x : "" )
+        })
+    
+    //計算總金額
+    let totalAmount = records.reduce((prev, record) => prev += record.amount, 0)
+
+    res.render('index', { 
+        records,
+        categories,
+        totalAmount,
+        keyWord
+    })
+    }catch (e) {
+    console.warn(e)
+    }
 })
 
 // 匯出路由模組
