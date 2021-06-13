d10cf5b85adb5aa8e27d0c40acade61d4240784f
danlin <iita71737@gmail.com>
2021-06-13 23:28:01 +0800

    modify seeds file use distructuring assignment

--- models/seeds/categorySeeder.js
+++ models/seeds/categorySeeder.js
@@ -1,21 +1,15 @@
 const Category = require('../category') // 載入 todo model
 const db = require('../../config/mongoose')
-const categories = require('../../records.json')
+const { category } = require('../../records.json')
 
 
 db.once('open',  async() => {
     try {   
-        categories.category.forEach( category => {
-            Category.create({
-                title:category.title,
-                cateName :  category.cateName,
-                cateIcon : category.cateIcon,
-                cateIcon3x : category.cateIcon3x
-            }).then(() => {
+            Category.create(category)
+                .then(() => {
                 console.log('cateseeder insert done')
                 console.log('database connection close')
                 process.exit()       
-            })
         })
     }
     catch (e) {
--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,23 +1,15 @@
 const Record = require('../record') // 載入 todo model
 const db = require('../../config/mongoose')
-const records = require('../../records.json')
+const { results } = require('../../records.json')
 
 db.once('open', async () => {
     try {
-        records.results.forEach(record => {
-
-            Record.create({
-                name: record.name,
-                category: record.category,
-                category_en: record.category_en,
-                date: record.date,
-                amount: record.amount
-            }).then(() => {
+            Record.create(results)
+                .then(() => {
                 console.log('record seeder insert done')
                 console.log('database connection close')
                 process.exit()
             })
-        })
         
     } catch (e) {
         console.warn(e)
